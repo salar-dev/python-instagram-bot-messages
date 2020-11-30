99b2fba6b5030625b6909228fc6415dc89f210bc
Salar Dev <72287943+salar-dev@users.noreply.github.com>
2020-11-30 20:58:34 +0300

    Add files via upload

---
+++ insta.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from time import sleep
+
+class InstaBot:
+    def __init__(self, username, password):
+        self.driver = webdriver.Chrome()
+        self.driver.get('https://www.instagram.com')
+        sleep(2)
+        self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/button[1]')\
+            .click()
+        sleep(2)
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input')\
+            .send_keys(username)
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input')\
+            .send_keys(password)
+        sleep(1)
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]/button')\
+            .click()
+        sleep(4)
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[2]/a')\
+            .click()
+        sleep(4)
+        self.driver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]')\
+            .click()
+        sleep(2)
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/div/div[2]/div/div/div[1]/div[3]/div/div/div/div/div[1]/a')\
+            .click()
+        sleep(2)
+        
+        x = 1
+        while x < 1000:
+            self.driver.find_element_by_xpath('/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea')\
+                .send_keys('Hello: ' + str(x))
+            sleep(1)
+            self.driver.find_element_by_xpath('/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[3]/button')\
+                .click()
+            x += 1
+
+        sleep(100)
+
+
+InstaBot('username', 'password')
